6a8b067104f2016174ffbc97514966f6ac21bd3c
Rafael Junior <rafael.jr92@hotmail.com>
2020-04-09 17:39:01 -0300

    conceito de array filter

---
+++ 04-lists/for-forin/3.filter.js
@@ -0,0 +1,51 @@
+const { obterPessoas } = require('./service')
+
+/**
+ * const item = {
+ *  nome: 'Rafa',
+ *  idade: 28,
+ * }
+ * 
+ * const { nome, idade } = item
+ * console.log(nome, idade)
+ */
+
+
+Array.prototype.meuFilter = function(callback) {
+  const lista = []
+  for (index in this) {
+    const item = this[index]
+    const result = callback(item, index, this)
+    //0, "", null, undefined === false
+    if (!result) continue;
+    lista.push(item)
+  }
+  return lista;
+}
+ async function main() {
+  try {
+    const { results } = await obterPessoas('')
+
+    //por padrão precisa retornar um booleano
+    //para informar se deve manter ou remover da lista
+    //true > mantem
+    //não encontrou = -1
+    //encontrou = posição no array
+    // const pokemonFogo = results.filter(function(item) {
+    //   const result = item.name.toLowerCase().indexOf('pid') !== -1
+    //   return result
+    // })
+    const pokemonFogo = results.meuFilter((item, index, lista) => {
+      console.log(`index: ${index}`, lista.length)
+      return item.name.toLowerCase().indexOf('char') !== -1
+    })
+
+    const names = pokemonFogo.map((pokemon) => pokemon.name)
+    console.log(names)
+
+  } catch (error) {
+    console.error('Deu ruim', error)
+  }
+ }
+
+ main()
