ee754793368a6d4747267a050ddbf31b8874b332
Rafael Junior <rafael.jr92@hotmail.com>
2020-04-12 12:51:56 -0300

    Novo projeto CRUD com testes

---
+++ 06-cli/test.js
@@ -0,0 +1,32 @@
+const {
+  deepEqual,
+  ok
+} = require('assert')
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: 'Flash',
+  poder: 'Speed',
+  id: 1
+}
+
+const database = require('./database')
+
+describe('Suite de manipulação de Herois', () => {
+  before(async() => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+  })
+  it('Deve pesquisar um herói usando arquivos', async() => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const [resultado] = await database.listar(expected.id)
+    deepEqual(resultado, expected)
+  })
+
+  it('deve cadastrar um herói, usando arquivos', async() => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+
+    deepEqual(actual, expected)
+  })
+
+})
